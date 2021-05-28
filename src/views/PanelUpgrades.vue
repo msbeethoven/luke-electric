fd87b6950775cbbd5e00ec53bf34d68e941b7d64
core <msbeethoven@users.noreply.github.com>
2021-05-28 02:08:51 -0400

    Added base for Panel Upgrades and the footer in it

---
+++ src/views/PanelUpgrades.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="panel-upgrades">
+    <h1>This is about panel upgrades</h1>
+    <Footer />
+  </div>
+</template>
+
+
+<script>
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'PanelUpgrades',
+  components: {
+    Footer
+  }
+}
+</script>
