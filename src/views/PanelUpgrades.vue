c7c5e9b8dbf4c7a4b305ad54bd3aa953b200ac25
core <msbeethoven@users.noreply.github.com>
2021-06-17 12:04:10 -0400

    Basic layout of panel updates page

--- src/views/PanelUpgrades.vue
+++ src/views/PanelUpgrades.vue
@@ -1,21 +1,61 @@
 <template>
   <div class="panel-upgrades">
-    <Header />
-    <h1>This is about panel upgrades</h1>
-    <Footer />
+    <div class="grid">
+      <div class="image-fill">
+        <img
+          src="/images/pliers.jpg"
+          alt="panel upgrades"
+        />
+      </div>
+      <div class="padding">
+        <h3> 
+          Make Sure Your Electrical System Is Safe and Efficient 
+        </h3>
+
+        <h5>
+          Choose us for electrical panel upgrades in Hampton & Clinton, NJ
+        </h5>
+
+        <p>
+          An outdated electrical panel can be dangerous, so it's important to
+          ensure that you have modern equipment. A professional electrician will
+          be able to help you find the right equipment for your property. Rely
+          on Luke Electric when you need an electrical panel replacement in
+          Hampton or Clinton, NJ. We can inspect your system and let you know if
+          an upgrade is the best choice. Schedule an electrical panel upgrade
+          today.
+        </p>
+
+        <h5>When you should replace your electrical panel</h5>
+
+        <p>
+          Old or inefficient panels will usually cause a variety of issues.
+          Reach out to us for electrical panel replacement services if:
+        </p>
+        <ul>
+          <li>You hear crackling from your panel</li>
+          <li>Your outlets are hot or look burnt</li>
+          <li>Your lights are flickering</li>
+        </ul>
+        <p>
+          Getting an electrical panel upgrade is also a good idea if you plan on
+          adding new outlets or appliances to your system. Call us today at
+          908-246-8546 if you're concerned about your equipment.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-
 <script>
-import Header from '@/components/TheHeader.vue'
-import Footer from '@/components/TheFooter.vue'
+import Header from "@/components/TheHeader.vue";
+import Footer from "@/components/TheFooter.vue";
 
 export default {
-  name: 'Contact',
+  name: "Contact",
   components: {
     Header,
-    Footer
-  }
-}
+    Footer,
+  },
+};
 </script>
